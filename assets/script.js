6f11150c5ed1735b533f39ed569a9b26de8a66c7
Jas James <jasdjames@gmail.com>
2021-01-09 20:58:43 -0600

    Added Open weather api, Search functions finds the city

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,22 @@ var api = "c5802144e43bf1301ef522550796e16e";
 
 
 $("#searchBtn").on("click", function (event) {
-var searchCity = $("#searchCity").val;
-console.log(searchCity);
+
  if ($(this)){
-  
-var openURL = `api.openweathermap.org/data/2.5/forecast?q=houston&appid=${api}`
+var searchCity =$("#searchCity").val();
+console.log(searchCity);
+var openURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${api}`
 $.ajax({
     url: openURL,
     method: "GET"
   }).then(function(response) {
       console.log(response);
+      console.log (response.city.name);
+    //   print the name of the city to the string
+    $("#cityName").html(`<h5>${response.city.name}</h5>`);
+
   })
 
  }
 })
+
