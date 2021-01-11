d1d2eb94b2ad735fa8c19d55279f19222ab0b522
Jas James <jasdjames@gmail.com>
2021-01-11 17:44:11 -0600

    added information to 5 day cards

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var DateTime = luxon.DateTime;
-var dt = DateTime.local();
+var Vt = DateTime.local();
 var Day = $("#dateToday")
-var newFormat = Day.text(dt.toLocaleString(DateTime.DATE_HUGE));
+var newFormat = Day.text(Vt.toLocaleString(DateTime.DATE_HUGE));
 var cityName = $("#cityName")
 var api = "c5802144e43bf1301ef522550796e16e";
 
@@ -36,11 +36,42 @@ $("#topText").empty().append(` Temperature: ${tempRound}°F `)
 // Icon
 var iconCode = (response.list[0].weather[0].icon)
 
+
 $("#topText").append(`<img id="icon" src="http://openweathermap.org/img/wn/${iconCode}@2x.png"/> `)
     // Humidity 
 $("#topText").append(`<p> Humidity: ${response.list[0].main.humidity}% </p>`) 
     // Windspeed 
 $("#topText").append(`<p> Windspeed: ${response.list[0].wind.speed} mph </p>`) 
+
+
+
+// Card 1 
+var dateArr = [response.list[0],response.list[8],response.list[16],response.list[24], response.list[32]];
+console.log(dateArr);
+console.log((DateTime.fromSeconds(dateArr[2].dt)));
+
+
+   
+// There is a better way to do this 
+// Will Utterback tried to  help me think of ways to loop through this code - I still am having trouble getting the loops to work 
+// $('card-title').each(function (el) { ? 
+// If data number === # in array --- put the replace the index in dateArr - How do I write that?
+$("#b1Title").empty().append((DateTime.fromSeconds(dateArr[0].dt)).toLocaleString(DateTime.DATE_HUGE))
+$("#b2Title").empty().append((DateTime.fromSeconds(dateArr[1].dt)).toLocaleString(DateTime.DATE_HUGE))
+$("#b3Title").empty().append((DateTime.fromSeconds(dateArr[2].dt)).toLocaleString(DateTime.DATE_HUGE))
+$("#b4Title").empty().append((DateTime.fromSeconds(dateArr[3].dt)).toLocaleString(DateTime.DATE_HUGE))
+$("#b5Title").empty().append((DateTime.fromSeconds(dateArr[4].dt)).toLocaleString(DateTime.DATE_HUGE))
+
+$("#b1Text").empty().append(`Temperature: ${(Math.round(dateArr[0].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[0].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[0].main.humidity}% </p> <p> Windspeed: ${dateArr[0].wind.speed} mph </p>`)
+$("#b2Text").empty().append(`Temperature: ${(Math.round(dateArr[1].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[1].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[1].main.humidity}% </p> <p> Windspeed: ${dateArr[1].wind.speed} mph </p>`)
+$("#b3Text").empty().append(`Temperature: ${(Math.round(dateArr[2].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[2].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[2].main.humidity}% </p> <p> Windspeed: ${dateArr[2].wind.speed} mph </p>`)
+$("#b4Text").empty().append(`Temperature: ${(Math.round(dateArr[3].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[3].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[3].main.humidity}% </p> <p> Windspeed: ${dateArr[3].wind.speed} mph </p>`)
+$("#b5Text").empty().append(`Temperature: ${(Math.round(dateArr[4].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[4].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[4].main.humidity}% </p> <p> Windspeed: ${dateArr[4].wind.speed} mph </p>`)
+
+
+
+// })
+
     // UV Index 
  var lat = (response.city.coord.lat)
  console.log((response.city.coord.lat));   
@@ -67,9 +98,9 @@ $("#uvBtn").removeClass().addClass("btn btn-danger disabled");
 }
 $("#uvBtn").empty().append(`UV Index: ${response.current.uvi} `)
 
-// figure out how to clear the uv index between clicks
 
-// make button yellow 
+
+
 
 
 
