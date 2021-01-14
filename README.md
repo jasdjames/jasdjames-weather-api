48012d39df2cccec2e0819543a001990fa001ed5
Jas James <jasdjames@gmail.com>
2021-01-13 22:43:04 -0600

    ReadME completed

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # jasdjames-weather-api
+## Description
+The intended purpose of this project to create a weather app with Open Weather API. The app gives the 5 day forecast and daily forecast for searched city. The sidebar holds the previous searches and can be reloaded by clicking on the name. 
 
-## Resources 
-[Bootstrap documentation help] (https://stackoverflow.com/questions/41574776/what-is-class-mb-0-in-bootstrap-4)
+## Installation
+This project is listed in the the GitHub repository at this is link [GitHub Repository] (https://github.com/jasdjames/jasdjames-weather-api)
+
+
+This page has been deployed here [Page] (https://jasdjames.github.io/jasdjames-weather-api/)
+
+![Photo](assets/images/weather.png) 
+
+## Usage
+The page functions mostly as it should. The names do not append to the "previous searches" exactly as I would have liked and as I would have completed with more time and research. 
+
+
+## Credits
+ Will Utterback has grown to someone that it is easy to collaborate with on projects. I bounces ideas off of him to see if they would work. He also helped me to see things in other ways. 
+
+## References
+
+![Luxon-YouTube] (https://www.youtube.com/watch?v=U4GJA7pY0O0&t=300s)
+
+Luxon and JQuery documentation 
+
+
 
 [Jquery.empty](https://api.jquery.com/empty/)
 
@@ -9,3 +31,14 @@
 
 [Stringify local storage ](https://stackoverflow.com/questions/17745292/how-to-retrieve-all-localstorage-items-without-knowing-the-keys-in-advance)
 [removing duplicates from array ](https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array)
+## License
+MIT License
+
+Copyright (c) [2020] [JasDJames]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+## Resources 
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function() {
     searchFunc(localStorage.getItem("value"))
-
+    
 })
 
 var DateTime = luxon.DateTime;
@@ -19,74 +19,61 @@ var cityArr2 = []
 $("#searchBtn").on("click", function () {
     console.log($(this));
     console.log($("#searchCity").val());
-    searchFunc($("#searchCity").val())
+ searchFunc($("#searchCity").val())
 })
-function searchFunc(searchCity) {
+ function searchFunc(searchCity) {
+    
+        
+        console.log(searchCity);
+        var openURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&units=imperial&appid=${api}`
+        $.ajax({
+            url: openURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.city.name);
+            //   print the name of the city to the string
+            $("#cityName").html(`<h5>${response.city.name}</h5>`);
 
-    // var searchCity = $("#searchCity").val();
-    console.log(searchCity);
-    var openURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&units=imperial&appid=${api}`
-    $.ajax({
-        url: openURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        console.log(response.city.name);
-        //   print the name of the city to the string
-        $("#cityName").html(`<h5>${response.city.name}</h5>`);
-
-        // Temp
-        console.log(response.list[0].main.temp)
-        var tempRound = Math.round(response.list[0].main.temp)
-        console.log(tempRound);
-        // $("#topText").empty();
-        $("#topText").empty().append(` Temperature: ${tempRound}°F `)
-        // Icon
-        var iconCode = (response.list[0].weather[0].icon)
-
-
-        $("#topText").append(`<img id="icon" src="http://openweathermap.org/img/wn/${iconCode}@2x.png"/> `)
-        // Humidity 
-        $("#topText").append(`<p> Humidity: ${response.list[0].main.humidity}% </p>`)
-        // Windspeed 
-        $("#topText").append(`<p> Windspeed: ${response.list[0].wind.speed} mph </p>`)
-
-        localStorage.setItem("value", searchCity)
+            // Temp
+            console.log(response.list[0].main.temp)
+            var tempRound = Math.round(response.list[0].main.temp)
+            console.log(tempRound);
+            // $("#topText").empty();
+            $("#topText").empty().append(` Temperature: ${tempRound}°F `)
+            // Icon
+            var iconCode = (response.list[0].weather[0].icon)
 
 
-        if (cityArr2.length > 5) {
-        } else {
-            cityArr2.push(localStorage.getItem("value"))
+            $("#topText").append(`<img id="icon" src="http://openweathermap.org/img/wn/${iconCode}@2x.png"/> `)
+            // Humidity 
+            $("#topText").append(`<p> Humidity: ${response.list[0].main.humidity}% </p>`)
+            // Windspeed 
+            $("#topText").append(`<p> Windspeed: ${response.list[0].wind.speed} mph </p>`)
+            
+            localStorage.setItem("value", searchCity)
+            
 
-            console.log(cityArr2);
-        }
-        //    $(".list-group-item").each(function() {
-        //     for (let i = 0; i< cityArr2.length; i++) {
-        //     console.log($(this));
-        //                 $(this).append(cityArr2)
-        //                 }  
+          if (cityArr2.length> 5){  
+          }else{
+          cityArr2.push(localStorage.getItem("value"))
 
-        //     })           
+           console.log(cityArr2);
+          }
+        
+                        
 
-
-
-        // $("#city1").append(cityArr2[0])
-        // $("#city2").append(cityArr2[1])
-        // $("#city3").append(cityArr2[2])
-        // $("#city4").append(cityArr2[3])
-        // $("#city5").append(cityArr2[4])
-        if (cityArr2.length > 5) {
-            cityArr2.splice(0, 1, searchCity)
-            console.log(cityArr2);
-
+            
+            if (cityArr2.length > 5) {
+                cityArr2.splice(0, 1, searchCity)
+                console.log(cityArr2);
+        
 
             $("#city1").text("").append(cityArr2[0])
-            // $("#city2").empty().append(cityArr2[1])
-            // $("#city3").empty().append(cityArr2[2])
-            // $("#city4").empty().append(cityArr2[3])
-            // $("#city5").empty().append(cityArr2[4])
+            
 
-        } else {
+
+            }else {
 
             $("#city1").text("").append(cityArr2[0])
             $("#city2").text("").append(cityArr2[1])
@@ -94,128 +81,80 @@ function searchFunc(searchCity) {
             $("#city4").text("").append(cityArr2[3])
             $("#city5").text("").append(cityArr2[4])
 
-        }
-
-
-        // console.log(cityArr)
-        // $("#city1").empty().append(cityArr[0])
-        // $("#city2").empty().append(cityArr[2])
-        // $("#city3").empty().append(cityArr[4])
-        // $("#city4").empty().append(cityArr[6])
-        // $("#city5").empty().append(cityArr[8])
-
-        // if (cityArr.length === 10) {
-        //     alert("STOP!!")
-        //    cityArr.splice(0, 1, ) 
-        // console.log(cityArr);
-
-
-
-
-        // console.log(JSON.stringify(localStorage));
-
-        // console.log(JSON.parse(JSON.stringify(localStorage)))
-        // console.log(cityArr[0].Vt);
-        $(".list-group-item").eq(0).addClass("blue")
-        // This is where you stopped - 1/12 8:18
-        // console.log(cityArr[0]);
-        // console.log(cityArr[0].index());
-
-        // if (cityArr!= [] ){
-        // $("#city1").append(cityArr[0].val)
-        // }
-
-        // Researched how to removed repetition in an array - Found an answer on stackoverflow - I place a link to this block of code in the resources to credit the author 
-
-
-        // var uniqueCity = [];
-        // $.each(cityArr, function(i, el){
-        //     if($.inArray(el, uniqueCity) === -1) uniqueCity.push(el);
-        // console.log(uniqueCity);
-        // });
-
-
-
-        // var printArr = $(printCity).val.makeArray()
-        // console.log(printArr);
-        // printArr.forEach(function (city) {
-
-        // $(".list-group-item").each(function () {
-        //     for (let i = 0; i < cityArr.length; i++) {
-        //         console.log(cityArr[i]);
-        //         $(".list-group-item").empty().append(cityArr[i])
-        //     }
-
-        //  })
-
-
-        // Card 1 
-        var dateArr = [response.list[0], response.list[8], response.list[16], response.list[24], response.list[32]];
-        console.log(dateArr);
-        console.log((DateTime.fromSeconds(dateArr[2].dt)));
-
-
-
-        // There is a better way to do this 
-        // Will Utterback tried to  help me think of ways to loop through this code - I still am having trouble getting the loops to work 
-        // $('card-title').each(function (el) { ? 
-        // If data number === # in array --- put the replace the index in dateArr - How do I write that?
-        $("#b1Title").empty().append((DateTime.fromSeconds(dateArr[0].dt)).toLocaleString(DateTime.DATE_HUGE))
-        $("#b2Title").empty().append((DateTime.fromSeconds(dateArr[1].dt)).toLocaleString(DateTime.DATE_HUGE))
-        $("#b3Title").empty().append((DateTime.fromSeconds(dateArr[2].dt)).toLocaleString(DateTime.DATE_HUGE))
-        $("#b4Title").empty().append((DateTime.fromSeconds(dateArr[3].dt)).toLocaleString(DateTime.DATE_HUGE))
-        $("#b5Title").empty().append((DateTime.fromSeconds(dateArr[4].dt)).toLocaleString(DateTime.DATE_HUGE))
-
-        $("#b1Text").empty().append(`Temperature: ${(Math.round(dateArr[0].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[0].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[0].main.humidity}% </p> <p> Windspeed: ${dateArr[0].wind.speed} mph </p>`)
-        $("#b2Text").empty().append(`Temperature: ${(Math.round(dateArr[1].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[1].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[1].main.humidity}% </p> <p> Windspeed: ${dateArr[1].wind.speed} mph </p>`)
-        $("#b3Text").empty().append(`Temperature: ${(Math.round(dateArr[2].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[2].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[2].main.humidity}% </p> <p> Windspeed: ${dateArr[2].wind.speed} mph </p>`)
-        $("#b4Text").empty().append(`Temperature: ${(Math.round(dateArr[3].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[3].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[3].main.humidity}% </p> <p> Windspeed: ${dateArr[3].wind.speed} mph </p>`)
-        $("#b5Text").empty().append(`Temperature: ${(Math.round(dateArr[4].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[4].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[4].main.humidity}% </p> <p> Windspeed: ${dateArr[4].wind.speed} mph </p>`)
-
-
-
-
-        // UV Index 
-        var lat = (response.city.coord.lat)
-        console.log((response.city.coord.lat));
-        var lon = (response.city.coord.lon)
-        console.log(lon);
-        var uvAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api}`
-        $.ajax({
-            url: uvAPI,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.current.uvi);
-
-
-            if (response.current.uvi < 3) {
-
-                // change button green
-                $("#uvBtn").removeClass().addClass("btn btn-success disabled");
-            } else if (response.current.uvi > 8) {
-                $("#uvBtn").removeClass().addClass("btn btn-danger disabled");
-
-            } else {
-                $("#uvBtn").removeClass().addClass("btn btn-warning disabled");
             }
-            $("#uvBtn").empty().append(`UV Index: ${response.current.uvi} `)
+
+            
+            
+         $(".list-group-item").eq(0).addClass("blue")  
+           
+
+            // Card 1 
+            var dateArr = [response.list[0], response.list[8], response.list[16], response.list[24], response.list[32]];
+            console.log(dateArr);
+            console.log((DateTime.fromSeconds(dateArr[2].dt)));
+
+
+
+            // There is a better way to do this 
+            // Will Utterback tried to  help me think of ways to loop through this code - I still am having trouble getting the loops to work 
+            // $('card-title').each(function (el) { ? 
+            // If data number === # in array --- put the replace the index in dateArr - How do I write that?
+            $("#b1Title").empty().append((DateTime.fromSeconds(dateArr[0].dt)).toLocaleString(DateTime.DATE_HUGE))
+            $("#b2Title").empty().append((DateTime.fromSeconds(dateArr[1].dt)).toLocaleString(DateTime.DATE_HUGE))
+            $("#b3Title").empty().append((DateTime.fromSeconds(dateArr[2].dt)).toLocaleString(DateTime.DATE_HUGE))
+            $("#b4Title").empty().append((DateTime.fromSeconds(dateArr[3].dt)).toLocaleString(DateTime.DATE_HUGE))
+            $("#b5Title").empty().append((DateTime.fromSeconds(dateArr[4].dt)).toLocaleString(DateTime.DATE_HUGE))
+
+            $("#b1Text").empty().append(`Temperature: ${(Math.round(dateArr[0].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[0].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[0].main.humidity}% </p> <p> Windspeed: ${dateArr[0].wind.speed} mph </p>`)
+            $("#b2Text").empty().append(`Temperature: ${(Math.round(dateArr[1].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[1].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[1].main.humidity}% </p> <p> Windspeed: ${dateArr[1].wind.speed} mph </p>`)
+            $("#b3Text").empty().append(`Temperature: ${(Math.round(dateArr[2].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[2].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[2].main.humidity}% </p> <p> Windspeed: ${dateArr[2].wind.speed} mph </p>`)
+            $("#b4Text").empty().append(`Temperature: ${(Math.round(dateArr[3].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[3].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[3].main.humidity}% </p> <p> Windspeed: ${dateArr[3].wind.speed} mph </p>`)
+            $("#b5Text").empty().append(`Temperature: ${(Math.round(dateArr[4].main.temp))}°F <img id="icon" src="http://openweathermap.org/img/wn/${dateArr[4].weather[0].icon}@2x.png"/> <p> Humidity: ${dateArr[4].main.humidity}% </p> <p> Windspeed: ${dateArr[4].wind.speed} mph </p>`)
+
+
+
+
+            // UV Index 
+            var lat = (response.city.coord.lat)
+            console.log((response.city.coord.lat));
+            var lon = (response.city.coord.lon)
+            console.log(lon);
+            var uvAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api}`
+            $.ajax({
+                url: uvAPI,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                console.log(response.current.uvi);
+
+
+                if (response.current.uvi < 3) {
+
+                    // change button green
+                    $("#uvBtn").removeClass().addClass("btn btn-success disabled");
+                } else if (response.current.uvi > 8) {
+                    $("#uvBtn").removeClass().addClass("btn btn-danger disabled");
+
+                } else {
+                    $("#uvBtn").removeClass().addClass("btn btn-warning disabled");
+                }
+                $("#uvBtn").empty().append(`UV Index: ${response.current.uvi} `)
 
 
 
 
 
 
+
+            })
 
         })
-
-    })
-
-
+    
+        
 }
 
-$(".list-group-item").on("click", function () {
+$(".list-group-item").on("click", function() {
     console.log($(this).val());
-    searchFunc($(this).text());
-
+ searchFunc($(this).text());
+ 
 })
